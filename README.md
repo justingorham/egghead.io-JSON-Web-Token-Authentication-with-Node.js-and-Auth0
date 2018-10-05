ace9e824e3d47872167780928c79c586c59709f2
Justin Gorham <justingorham@gmail.com>
2018-10-05 19:37:06 -0400

    08 - Connect a Front-End to a Secure API using JWTs

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 (4) Add a POST Route To Express And Parse the Body\
 (5) Provide Users With A JSON Web Token\
 (6) Allow CORS in Node.js and Express\
-(7) Authenticate Users With JWT for Access to Protected Resources
+(7) Authenticate Users With JWT for Access to Protected Resources\
+(8) Connect a Front-End to a Secure API using JWTs
--- api.js
+++ api.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const expressjwt = require('express-jwt');
+const serveStatic = require('serve-static')
 
 const app = express();
 const PORT = process.env.API_PORT || 8888;
@@ -26,6 +27,8 @@ const users = [{
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use(serveStatic('public'))
 
 app.get("/resource", (req, res) => {
     res.status(200).send("Public resource, you can see this")
