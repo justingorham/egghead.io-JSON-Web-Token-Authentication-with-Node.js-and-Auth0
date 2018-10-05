5c7a79c01537e00a67a0152a09f46e8ec9cac361
Justin Gorham <justingorham@gmail.com>
2018-10-04 23:38:17 -0400

    05 Provide Users With A JSON Web Token

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
 const app = express();
 const PORT = process.env.PORT || 8888;
+const SECRET = process.env.SECRET || "mysupersecretkey";
+
+const users = [{
+        id: 1,
+        username: "admin",
+        password: "admin"
+    },
+    {
+        id: 2,
+        username: "guest",
+        password: "guest"
+    }
+];
 
 app.use(bodyParser.json());
 
@@ -16,11 +31,32 @@ app.get("/status", (req, res) => {
 
 app.post("/login", (req, res) => {
     const {
-        username
+        username,
+        password
     } = req.body;
-    res
-        .status(200)
-        .send(`You logged in with ${username}`)
+    if (!username || !password) {
+        return res
+            .status(400)
+            .send("You need a username and password");
+    }
+
+    Promise.resolve(users.find((u) => {
+            return u.username === username && u.password === password;
+        }))
+        .then(user => {
+            if (!user) {
+                return res.status(401).send("User not found");
+            }
+            const access_token = jwt.sign({
+                sub: user.id,
+                username: user.username
+            }, SECRET, {
+                expiresIn: "3 hours"
+            });
+
+            res.status(200).send({access_token})
+        })
+
 })
 
 app.get("*", (req, res) => {
