4ef5267a5942fd7e6777201a2305c6c1e817f83c
Justin Gorham <justingorham@gmail.com>
2018-10-04 22:51:54 -0400

    03 - Set the Server Port in Express Using Environment Variables

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # JSON Web Token (JWT) Authentication with Node.js and Auth0 
 
 (2) Setup a Web Server in NOde.js using Express
+(3) Set the Server Port in Express Using Environment Variables
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
-const PORT = 8888;
+const PORT = process.env.PORT || 8888;
 
 app.get("/status",(req,res)=>{
     const localTime = (new Date()).toLocaleTimeString();
@@ -16,4 +17,4 @@ app.get("*",(req, res)=>{
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
-})
+});
